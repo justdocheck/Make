f1ad137f46e320ee59330e1938c19771b1099260
justdocheck <justdocheckcrazy@gmail.com>
2026-03-17 16:42:06 +0530

    Just_for_tutorial

---
+++ For_tutorial/script.js
@@ -0,0 +1,13 @@
+
+let head = document.getElementById("heading")
+let para = document.getElementById("paragraph")
+let button_B = document.getElementById("button_A")
+
+button_B.onclick = function(){
+    head.style.backgroundColor = "aqua"
+    para.style.color = "orange"
+    button_B.style.color = "green" 
+            
+}
+
+
